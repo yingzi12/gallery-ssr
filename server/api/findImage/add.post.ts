4ac80e9b980a121ce4a25a214c28f7d03a56f485
卢煌 <luhuang@jtexpress.com>
2023-11-13 12:44:06 +0800

    解决提交错误的问题

--- server/api/findImage/add.post.ts
+++ server/api/findImage/add.post.ts
@@ -5,13 +5,12 @@ export default defineEventHandler(async (event) => {
     // console.log(body)
 
     // Use the GET parameters to make a GET request to `/album/list`
-    const response = await fetch(`http://127.0.0.1:8098/findImage/add`,{
+    const response = await fetch(`https://admin.aiavr.uk/findImage/add`,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-           body: JSON.stringify(body
-        )
+           body: JSON.stringify(body)
     }
     );
     const dataJson = await response.json();

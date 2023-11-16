4072d29e514a6be4e89b034d263d05e0c3a01f60
卢煌 <luhuang@jtexpress.com>
2023-11-16 11:30:10 +0800

    优化地图

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -4,8 +4,8 @@ export default defineSitemapEventHandler(async () => {
     try {
         let urls=[];
         for(let i=1;i<20;i++) {
-            const response = await fetch(config.public.baseUrl+`/album/list?pageNum=`+i.toString());
-            console.log(config.public.baseUrl+`/album/list?page=`+i.toString())
+            const response = await fetch(config.public.baseUrl+`/album/list?pageNum=`+i.toString()+`&title=秀人网`);
+            // console.log(config.public.baseUrl+`/album/list?page=`+i.toString()+`&title=秀人网`)
 
             const dataJson = await response.json();
             // console.log(dataJson.data)
@@ -20,7 +20,23 @@ export default defineSitemapEventHandler(async () => {
             }
         }
         for(let i=1;i<20;i++) {
-            console.log(config.public.baseUrl+`/album/listSee?page=`+i.toString())
+            const response = await fetch(config.public.baseUrl+`/album/list?pageNum=`+i.toString());
+            // console.log(config.public.baseUrl+`/album/list?page=`+i.toString())
+
+            const dataJson = await response.json();
+            // console.log(dataJson.data)
+            if (dataJson.code == 200) {
+                // 假设API返回的是URL数组
+                urls.push(...dataJson.data.map(item => ({
+                    loc: `/detail?aid=${item.id}`,
+                    _sitemap: 'pages',
+                })));
+            } else {
+                console.error('API Error:', dataJson.message);
+            }
+        }
+        for(let i=1;i<20;i++) {
+            // console.log(config.public.baseUrl+`/album/listSee?page=`+i.toString())
             const response = await fetch(config.public.baseUrl+`/album/listSee?pageNum=`+i.toString());
             const dataJson = await response.json();
             // console.log(dataJson.data)

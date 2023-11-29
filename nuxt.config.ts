d831a96d5e2ff989f6ae633797ae184cf708268c
卢煌 <luhuang@jtexpress.com>
2023-11-29 12:33:43 +0800

    优化地图

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
   meta: {
     title: '图集网-图片,美女,写真,图集',
     meta: [
+          { name: 'content-language', content: 'zh-CH' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover' },
       { hid: 'keywords', name: 'keywords', content: "图片,美女,写真,图集" },
       { hid: 'description', name: 'keywords', content: "图集网 美女 写真 摄影 秀人网 Photo Gallery, Beauty, Photo, Photography, Showman.com" },

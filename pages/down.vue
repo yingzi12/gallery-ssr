36adf2b67068f9315b22bf105d2442dcb193ff85
卢煌 <luhuang@jtexpress.com>
2023-12-03 12:23:31 +0800

    优化地图

--- pages/down.vue
+++ pages/down.vue
@@ -16,9 +16,10 @@
           <div class="yw-hd-slide-con app-yhsc">
             <h2 class="yw-hd-slide-h app-yhsh yw-font">海量图片</h2>
             <p class="yw-hd-slide-p app-yhsp">美女写真汇集，各种类型的美女写真图片</p>
+            <p class="yw-hd-slide-p app-yhsp">用户自建图集，用户自行定价，用户自定会员</p>
             <p class="yw-hd-slide-p app-yhsp">电脑站、手机站、app，多种终端无广告</p>
             <div class="yw-hd-app-dld">
-              <a href="https://www.aiavr.uk/Album-Gallery-1.0.0.apk" class="yw-hd-app-btn" target="_blank"><i class="icon-android"></i>安卓版下载</a>
+              <a href="https://www.aiavr.uk/Album-Gallery-1.0.1.apk" class="yw-hd-app-btn" target="_blank"><i class="icon-android"></i>安卓版下载</a>
               <div class="qr-box" onmouseover="this.className='qr-box cur'" onmouseout="this.className='qr-box'">
 <!--                <div class="load-qr-pic qr-qd-pic jsPicQr">-->
 <!--                  <img src="https://www.51xs.uk/core/qrcode.php?string=https://www.51xs.uk/app/?my=down">-->

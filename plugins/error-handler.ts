b5c78c17bcebcb890bc022e8a408e49a12e3a0cc
卢煌 <luhuang@jtexpress.com>
2024-01-17 20:46:47 +0800

    更新package

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,13 +1,12 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    console.log("------------error handler--")
+   // console.log("------------error handler--")
     nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-        console.log("--------------errorHandler")
+       // console.log("--------------errorHandler")
         // handle error, e.g. report to a service
     }
-
     // Also possible
     nuxtApp.hook('vue:error', (error, instance, info) => {
-        console.log("--------------error")
+      //  console.log("--------------error")
         // handle error, e.g. report to a service
     })
 

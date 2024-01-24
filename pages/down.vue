57fba2adfdbb88ce1f9dc146d42691a4a75d6f40
卢煌 <luhuang@jtexpress.com>
2024-01-24 18:59:40 +0800

    修改下载方式

--- pages/down.vue
+++ pages/down.vue
@@ -9,7 +9,7 @@
             <p class="yw-hd-slide-p app-yhsp">用户自建图集，用户自行定价，用户自定会员</p>
             <p class="yw-hd-slide-p app-yhsp">电脑站、手机站、app，多种终端无广告</p>
             <div class="yw-hd-app-dld">
-              <a href="https://image.51x.uk/xinshijie/gallery_album_1_1.apk" class="yw-hd-app-btn" target="_blank"><i class="icon-android"></i>安卓版下载 版本号：2.0.1</a>
+              <a href="https://image.51x.uk/xinshijie/gallery_album_1_1.apk" class="yw-hd-app-btn" target="_blank"><i class="icon-android"></i>安卓版下载 最新版本号：2.0.1 更新时间:2024-01-24 </a>
               <div class="qr-box" onmouseover="this.className='qr-box cur'" onmouseout="this.className='qr-box'">
                 <a class="qq-qr-icon jsLoadQr"><i class="icon-qr"></i></a>
               </div>

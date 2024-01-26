cb2742dfcac7f909a98fcda9a5fc5ed4fd959a5d
卢煌 <luhuang@jtexpress.com>
2024-01-26 12:28:09 +0800

    修改下载方式

---
+++ server/api/userAlbum/error.get.ts
@@ -0,0 +1,15 @@
+import {tansParams} from "~/server/utils/urlUtils";
+
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig();
+    const query = getQuery(event)
+
+    // Use the GET parameters to make a GET request to `/album/list`
+    const response = await fetch(config.public.baseUrl+`/userAlbum/error?id=`+query.id);
+    const dataJson = await response.json();
+    // //console.log(dataJson.data)
+    return {
+        code:dataJson.code,
+        data: dataJson.data,
+    };
+});

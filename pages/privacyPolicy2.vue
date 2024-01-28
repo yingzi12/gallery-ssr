c17288fda1db9b13cd03882107d17adcfc8e1e11
卢煌 <luhuang@jtexpress.com>
2024-01-28 15:42:22 +0800

    修改下载方式

---
+++ pages/privacyPolicy2.vue
@@ -0,0 +1,10 @@
+<template>
+    <privacy-policy></privacy-policy>
+</template>
+<script setup>
+import PrivacyPolicy from "~/pages/privacyPolicy.vue";
+definePageMeta({
+  layout: false,
+});
+</script>
+
---
+++ pages/use2.vue
@@ -0,0 +1,10 @@
+<template>
+  <use></use>
+</template>
+<script setup>
+import Use from "~/pages/use.vue";
+definePageMeta({
+  layout: false,
+});
+</script>
+
